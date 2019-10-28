81acb5b3d2fa93c52378e50ed018eb5b77714fca
hy.Yang <yanghaiyang@yudao365.com>
2019-10-28 17:39:33 +0800

    init

---
+++ src/main/java/com/fhlkd/Springcloud02Application.java
@@ -0,0 +1,15 @@
+package com.fhlkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Springcloud02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springcloud02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/fhlkd/Springcloud02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhlkd;
+
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Springcloud02ApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
